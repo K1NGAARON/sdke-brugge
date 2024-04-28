6540070e9c08dd4c6537c5227bda55b6aa8fc533
K1NGAARON <gerardaaron1997@gmail.com>
2024-04-28 13:39:20 +0200

    changers

---
+++ home/js/splide.js
@@ -0,0 +1,7 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    new Splide( '#splide-test', {
+        type: 'slide',
+        perPage: 3,
+        perMove: 1,
+    }).mount();
+});
